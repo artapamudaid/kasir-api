126492b9746d87f1a188779fceffae088ec2ed6b
Arta Tri Pamuda <pamuda.id@gmail.com>
2026-02-12 15:18:23 +0700

    fix: correct CORS header name from `Access-Control-Allow-Method` to `Access-Control-Allow-Methods`

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Method", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "X-API-Key, Content-Type")
 
 		if r.Method == "OPTIONS" {
